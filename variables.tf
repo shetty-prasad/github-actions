cbba5da69e23629e7d082bc466bba317dd31913f
shetty4885 <shetty4885@yahoo.co.in>
2024-07-23 17:36:35 +0530

    added TF files

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "region" {
+  type    = string
+  default = "ap-south-1"
+}
+
+# variable "access_key" {
+#     type = string
+
+# }
+
+# variable "secret_key" {
+#     type = string
+
+# }
